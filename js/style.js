810da8968a961efc3824e454278e0789e5728699
JorgeMJ <jorge.joven1@gmail.com>
2020-04-04 14:21:38 -0400

    Added page structure and style

--- js/style.js
+++ js/style.js
@@ -1,1 +1,35 @@
-/* javascript effects */
+function getBtnToggle(){
+	var x = document.getElementById("getRecipes");
+
+	if (x.style.display !== "block"){
+		x.style.display = "block";
+	}
+
+	else{
+		x.style.display = "none";
+	}
+}
+
+function addBtnToggle(){
+	var x = document.getElementById("addRecipes");
+
+	if (x.style.display !== "block"){
+		x.style.display = "block";
+	}
+
+	else{
+		x.style.display = "none";
+	}
+}
+
+function showDescription(id){
+	var x = document.getElementById(`descript${id}`); 
+
+	if (x.style.display !== "block"){
+		x.style.display = "block";
+	}
+
+	else{
+		x.style.display = "none";
+	}
+}
