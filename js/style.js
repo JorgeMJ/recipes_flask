ae5156963dbd616983b8fe651eebd20b2cd41247
JorgeMJ <jorge.joven1@gmail.com>
2020-04-06 13:54:07 -0400

    style modifications

--- js/style.js
+++ js/style.js
@@ -1,18 +1,22 @@
-/* *DESCRIPTION* */
+/* 
+File:        js/style.js
+Description: It contains the JavaScript functions to toggle the buttons to get recipes (getBtnRecipes),
+             add recipes (adddBtnRecipes), and show the description of recipes (showDescription)
+*/
+
 function toggle(htmlItem){
 	var x = document.getElementById(htmlItem);
 
 	if (x.style.display !== "block"){
 		x.style.display = "block";
 	}
-
 	else{
 		x.style.display = "none";
 	}
 }
 
 function getBtnToggle(){
-	toggle("getRecipes");
+    toggle("getRecipes");
 }
 
 function addBtnToggle(){
@@ -20,5 +24,5 @@ function addBtnToggle(){
 }
 
 function showDescription(id){
-	toggle(`descript${id}`); 
+    toggle(`descript${id}`); 
 }
