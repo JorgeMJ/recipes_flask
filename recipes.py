1d907c4c0c401c4280cfa0e4a1dca77a54e08200
JorgeMJ <jorge.joven1@gmail.com>
2020-04-08 23:52:51 -0400

    style html and created submit function

--- recipes.py
+++ recipes.py
@@ -1,15 +1,31 @@
 ''' Main function that will call the necessary functions '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        
+        name = request.form['nameIn']
+        kind = request.form['kindIn']
+        time = request.form['timeIn']
+        ingredients = request.form['ingredientsIn']
+        description = request.form['descriptionIn']
+       
+        print(name, kind, time, ingredients, description)
+        return('Success')
+        
+
+
 
 if __name__ == "__main__":
-	
-	app.run(debug = True)
+    app.debug = True
+    app.run()
 
 
