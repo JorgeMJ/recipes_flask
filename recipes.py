a14115f2e652fa86597411ad438570b0b5977bf7
JorgeMJ <jorge.joven1@gmail.com>
2020-04-09 19:00:27 -0400

    Add flash success message on submit recipe

--- recipes.py
+++ recipes.py
@@ -1,8 +1,10 @@
 ''' Main function that will call the necessary functions '''
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
+#required by 'flash' becasue it works based on cookies
+app.secret_key = 'my secret key'
 
 @app.route('/')
 def index():
@@ -17,14 +19,15 @@ def submit():
         time = request.form['timeIn']
         ingredients = request.form['ingredientsIn']
         description = request.form['descriptionIn']
-       
+        
+        flash('Success! Your recipe has been added.')
         print(name, kind, time, ingredients, description)
-        return('Success')
+        return render_template('index.html')
         
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(debug=True)
 
 
